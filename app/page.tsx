83d595fd80f1df2abde09c52a660c5fb80ef5c35
Pradip Chaudhary <chaudharypradip678@gmail.com>
2026-01-29 00:31:22 +0545

    Clean UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
+import Image from "next/image";
 
-export default function Home() {
+export default function ElectionHero() {
   return (
-    <>
-      Hello World
-    </>
+    <div className="relative w-screen h-screen flex items-center justify-center bg-gray-50 overflow-hidden">
+
+      {/* Centered Banner Image */}
+      <div className="relative w-[300px] sm:w-[400px] md:w-[500px] lg:w-[600px] aspect-[2/3]">
+        <Image
+          src="/images/35.png"
+          alt="Election Banner"
+          fill
+          className="object-contain"
+          priority
+        />
+      </div>
+
+    </div>
   );
 }
