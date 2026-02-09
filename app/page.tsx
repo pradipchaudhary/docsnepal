9a67577c1339be73e30dc8b71c57aa20f19ba183
Pradip Chaudhary <chaudharypradip678@gmail.com>
2026-02-09 14:22:28 +0545

    Remove NepaliCalendar from HomePage
    Removed NepaliCalendar component from HomePage.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
-import NepaliCalendar from '@/components/NepaliCalendar'
 import React from 'react'
 
 const HomePage = () => {
@@ -9,7 +8,6 @@ const HomePage = () => {
     <div>
       <Header />
       <Hero />
-      <NepaliCalendar />
       <Footer />
     </div>
   )
